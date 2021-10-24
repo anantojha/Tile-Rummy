5f7bace8791698acfc55fa41688f052f1c27dc27
Anant Ojha <anantojha@cmail.carleton.ca>
2021-10-24 01:43:41 -0400

    ADDED COMMENTS FOR step definitions classes

--- src/test/java/org/a1/NetworkingStepDefs.java
+++ src/test/java/org/a1/NetworkingStepDefs.java
@@ -8,11 +8,17 @@ import junit.framework.TestCase;
 import java.util.ArrayList;
 
 public class NetworkingStepDefs extends TestCase {
-    private GameServer server;
-    private ArrayList<Player> players = new ArrayList<>();
-    private ArrayList<Thread> threads = new ArrayList<>();
+    private GameServer server;  // Game Server
+    private ArrayList<Player> players = new ArrayList<>();  // List of Players
+    private ArrayList<Thread> threads = new ArrayList<>();  // List of Game Server Threads
 
-    @Given("the server is running on port {int}")
+    /*
+     *  Starting the Game Server
+     * - Connect to a PORT
+     * - Accept incoming connections from players
+     * - Start the Game Loop
+     */
+    @Given("Game Server is running on port {int}")
     public void start_server(Integer port) {
         new Thread(new Runnable() {
             @Override
@@ -29,6 +35,11 @@ public class NetworkingStepDefs extends TestCase {
         }).start();
     }
 
+    /*
+     *  Starting the Player Client
+     * - Initialize new Player
+     * - Connect Player to server at PORT with a NAME
+     */
     @When("I connect to the server on port {int} with name {string}")
     public void connect_a_player_to_server_at_port(int port, String name) {
         threads.add(new Thread(new Runnable() {
@@ -41,6 +52,10 @@ public class NetworkingStepDefs extends TestCase {
         }));
     }
 
+    /*
+     *  Player Client is connected to Game Server
+     * - Start Game Server Thread for Player
+     */
     @Then("Player {int} is connected to server")
     public void player_is_connected_to_server(int player) throws InterruptedException {
         threads.get(threads.size()-1).start();
--- src/test/java/org/a1/PlayingRoundStepDefs.java
+++ src/test/java/org/a1/PlayingRoundStepDefs.java
@@ -10,10 +10,16 @@ import junit.framework.TestCase;
 import java.util.ArrayList;
 
 public class PlayingRoundStepDefs extends TestCase {
-    private GameServer gs;
-    private Game game;
+    private GameServer gs;  // Game Server
+    private Game game;      // Game
 
-    @Given("Game is started")
+    /*
+     *  Starting A Game
+     * - Create Game Server
+     * - Create Players
+     * - Create Tiles
+     */
+    @Given("Round is started")
     public void start_game(){
         gs = new GameServer(true);
         game = new Game();
@@ -26,11 +32,19 @@ public class PlayingRoundStepDefs extends TestCase {
         gs.tiles = game.generateTiles();
     }
 
+    /*
+     *  Start Player's First Turn
+     * - Set Initial 30 Melds to false
+     */
     @And("Player {int} first turn")
     public void players_first_turn(int player){
         gs.players[player-1].setInitial30Meld(false);
     }
 
+    /*
+     *  Create Player's Hand
+     * - Add input tiles to Player's hand
+     */
     @And("Player {int} hand is {string}")
     public void set_play_hands(int player, String hand){
         for(String s : hand.replaceAll("\\s", "").replaceAll("}","").replaceAll("\\{","").split(",")) {
@@ -39,6 +53,10 @@ public class PlayingRoundStepDefs extends TestCase {
         game.resetPreviouslyMovedTile(gs.players, gs.tiles, gs.melds);
     }
 
+    /*
+     *  Player Draws a Tile from the Table
+     * - Player requests to get a new tile from table
+     */
     @When("Player {int} draws new tile")
     public void player_draws_new_tile(int player){
         if(gs.players[player-1].getHand().size() == 0) {
@@ -48,27 +66,42 @@ public class PlayingRoundStepDefs extends TestCase {
         game.drawNewTile(gs.players[player-1], gs.tiles);
     }
 
+    /*
+     *  Player Plays a Meld(s)
+     * - Player requests to meld(s) to the table
+     * - Both new melds and melds created from reusing tiles
+     */
     @When("Player {int} plays {string}")
     public void player_plays_melds(int player, String melds){
         game.resetPreviouslyMovedTile(gs.players, gs.tiles, gs.melds);
         game.playMelds(gs.players[player-1], gs.melds, game.convertMeldInputToTiles(gs.players[player-1].processInputMelds(melds)));
     }
 
+    /*
+     *  Player's Hand is Updated
+     * - Player's hand reflects moves the player has made
+     */
     @Then("Player {int} hand is updated")
     public void player_hand_updated(int player) throws InterruptedException {
-        Thread.sleep(100);
         game.printHand(gs.players[player-1]);
     }
 
+    /*
+     *  Table is Updated
+     * - Table reflects the moves all player have made
+     * - Melds on the table are updated
+     * - Unused tiles on table are updated
+     */
     @And("Table is updated")
     public void table_updated() throws InterruptedException {
-        Thread.sleep(100);
         game.printMelds(gs.melds);
         game.printRemainingTiles(gs.tiles);
     }
 
-
-
+    /*
+     *  Joker is used to Create a Meld
+     * - All possible tiles a Joker can represent in a valid Meld
+     */
     @And("Joker is used as another tile")
     public void player_plays_joker(){
         for(ArrayList<Tile> m : gs.melds){
